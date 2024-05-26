bb76e29750b282bf458c49f562f5e82cd993469f
m333ga <megahedgeyt@gmail.com>
2024-05-26 13:28:46 +0500

    26-05-2024 08:28

--- js/fit-textarea-to-text-script.js
+++
@@ -1,5 +0,0 @@
-// function fitTextareaToText() {
-//   const textarea = document.getElementById("input-container-textaria-main");
-//   textarea.style.height = "0px";
-//   textarea.style.height = textarea.scrollHeight + 25 + "px";
-// }
